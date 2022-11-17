574ef6934dea8bc57e4606662cb1ebb5c3f5d2a5
Amandeep <anath0610@conestogac.on.ca>
2022-11-17 17:29:44 -0500

    Initial commit of assignment 4. The files include lab 6 and 7 but this is the start of A4.

--- an_asm.s
+++ an_asm.s
@@ -1,3 +1,21 @@
+.data
+
+lab_ticks: .word 0
+lab_ticks_blinks: .word 0
+
+.text
+
+
+@ Define magic numbers as constants
+.equ    I2C_Address, 0x32
+.equ    X_HI, 0x29
+.equ    Y_HI, 0x2B
+.equ    READ_DELAY, 0xFFFFF
+
+
+
+
+
 @ Test code for my own new function called from C
 @ This is a comment. Anything after an @ symbol is ignored.
 @@ This is also a comment. Some people use double @@ symbols.
@@ -16,13 +34,214 @@
     .syntax unified                     @ Sets the instruction set to the new unified ARM + THUMB
                                         @ instructions. The default is divided (separate instruction sets)
 
-    .global a3_Game                 @ Make the symbol name for the function visible to the linker
+    .global lab7_tick                 @ Make the symbol name for the function visible to the linker
 
     .code 16                            @ 16bit THUMB code (BOTH .code and .thumb_func are required)
     .thumb_func                         @ Specifies that the following symbol is the name of a THUMB
                                         @ encoded function. Necessary for interlinking between ARM and THUMB code.
 
-    .type a3_Game, %function     @ Declares that the symbol is a function (not strictly required)
+    .type lab7_tick, %function     @ Declares that the symbol is a function (not strictly required)
+
+
+lab7_tick:
+
+
+    ldr     r1, =lab_ticks                  @ get the address of lab ticks
+    str     r0, [r1]                        @ store contents of r0 to address of lab ticks
+
+
+    bx      lr                             @ Return (Branch eXchange) to the address in the link register (lr)
+
+
+
+
+
+
+
+
+
+
+@ Test code for my own new function called from C
+@ This is a comment. Anything after an @ symbol is ignored.
+@@ This is also a comment. Some people use double @@ symbols.
+    .code 16                            @ This directive selects the instruction set being generated.
+                                        @ The value 16 selects Thumb, with the value 32 selecting ARM.
+
+    .text                               @ Tell the assembler that the upcoming section is to be considered
+                                        @ assembly language instructions - Code section (text -> ROM)
+
+
+
+@@ Function Header Block
+    .align 2                            @ Code alignment - 2^n alignment (n=2)
+                                        @ This causes the assembler to use 4 byte alignment
+
+    .syntax unified                     @ Sets the instruction set to the new unified ARM + THUMB
+                                        @ instructions. The default is divided (separate instruction sets)
+
+    .global _an_lab_setup                 @ Make the symbol name for the function visible to the linker
+
+    .code 16                            @ 16bit THUMB code (BOTH .code and .thumb_func are required)
+    .thumb_func                         @ Specifies that the following symbol is the name of a THUMB
+                                        @ encoded function. Necessary for interlinking between ARM and THUMB code.
+
+    .type _an_lab_setup, %function     @ Declares that the symbol is a function (not strictly required)
+
+
+_an_lab_setup:
+
+
+    ldr     r1, =lab_ticks                  @ get the address of lab ticks
+    str     r0, [r1]                        @ store contents of r0 to address of lab ticks
+
+
+
+    bx      lr                             @ Return (Branch eXchange) to the address in the link register (lr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@ Test code for my own new function called from C
+@ This is a comment. Anything after an @ symbol is ignored.
+@@ This is also a comment. Some people use double @@ symbols.
+    .code 16                            @ This directive selects the instruction set being generated.
+                                        @ The value 16 selects Thumb, with the value 32 selecting ARM.
+
+    .text                               @ Tell the assembler that the upcoming section is to be considered
+                                        @ assembly language instructions - Code section (text -> ROM)
+
+
+
+@@ Function Header Block
+    .align 2                            @ Code alignment - 2^n alignment (n=2)
+                                        @ This causes the assembler to use 4 byte alignment
+
+    .syntax unified                     @ Sets the instruction set to the new unified ARM + THUMB
+                                        @ instructions. The default is divided (separate instruction sets)
+
+    .global _an_lab_tick                 @ Make the symbol name for the function visible to the linker
+
+    .code 16                            @ 16bit THUMB code (BOTH .code and .thumb_func are required)
+    .thumb_func                         @ Specifies that the following symbol is the name of a THUMB
+                                        @ encoded function. Necessary for interlinking between ARM and THUMB code.
+
+    .type _an_lab_tick, %function     @ Declares that the symbol is a function (not strictly required)
+
+
+@ Function Declaration : int _an_lab_tick(int lab7_input)
+@
+@ Input: r0 (i.e. r0 holds a value)
+@ Returns: r0
+@
+
+@ Here is the actual function
+
+_an_lab_tick:
+
+    push    {r4-r7, lr}                     @ Put aside registers we want to restore later
+    
+
+    ldr     r1, =lab_ticks                  @ get the address of lab ticks
+    ldr     r0, [r1]                        @ load current ticks value 
+
+    subs    r0, r0, #1                      @ subtract 1 from the ticks
+    ble     out                             @ go to out if it is 0
+
+
+    str     r0, [r1]                        @ store value back into r1
+
+    ldr     r1, =lab_ticks_blinks           @ get address of lab ticks blinks
+    ldr     r0, [r1]                        @ load current ticks value
+
+    subs    r0, r0, #1                      @ subtract 1 from ticks
+    str     r0, [r1]                        @ store the current decremented count
+
+    bgt     out                             @ go to out if the tick isn't 0
+
+    
+    mov     r0, #I2C_Address                @ copy I2C address into r0
+    mov     r1, #X_HI                       @ use r1 to hold high bit value of accelerometer
+    bl      COMPASSACCELERO_IO_Read         @ call COMPASSACCELERO_IO_Read to read input of accelerometer
+    sxtb    r0, r0                          @ extend the signed 8 bit value in r0 to be a signed 32 bit value
+
+    mov     r0, #1                          @ copy LED index 1 to r0
+    bl      BSP_LED_Toggle                  @ call BSP_LED_Toggle to turn LED on/off
+
+
+    ldr     r1, =lab_ticks_blinks           @ get address of lab ticks blinks
+    mov     r0, #250                        @ reset the tick value
+    str     r0, [r1]                        @ store current value back into r1
+
+
+    @mov     r0, #0                          @ copy 0 to r0 to turn on LED 0
+    @bl      BSP_LED_Toggle                  @ call bsp led toggle to turn on LED
+    @ldr     r1, =lab_ticks_blinks           @ get address of lab ticks blinks
+    @mov     r0, #500                        @ reset the tick value
+    @str     r0, [r1]                        @ store current value back into r1
+
+
+
+
+   
+
+
+out:
+
+
+    pop     {r4-r7, lr}                    @ Bring all the register values back
+
+    bx      lr                             @ Return (Branch eXchange) to the address in the link register (lr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@ Test code for my own new function called from C
+@ This is a comment. Anything after an @ symbol is ignored.
+@@ This is also a comment. Some people use double @@ symbols.
+    .code 16                            @ This directive selects the instruction set being generated.
+                                        @ The value 16 selects Thumb, with the value 32 selecting ARM.
+
+    .text                               @ Tell the assembler that the upcoming section is to be considered
+                                        @ assembly language instructions - Code section (text -> ROM)
+
+
+
+@@ Function Header Block
+    .align 2                            @ Code alignment - 2^n alignment (n=2)
+                                        @ This causes the assembler to use 4 byte alignment
+
+    .syntax unified                     @ Sets the instruction set to the new unified ARM + THUMB
+                                        @ instructions. The default is divided (separate instruction sets)
+
+    .global lab6                 @ Make the symbol name for the function visible to the linker
+
+    .code 16                            @ 16bit THUMB code (BOTH .code and .thumb_func are required)
+    .thumb_func                         @ Specifies that the following symbol is the name of a THUMB
+                                        @ encoded function. Necessary for interlinking between ARM and THUMB code.
+
+    .type lab6, %function     @ Declares that the symbol is a function (not strictly required)
 
 
 @ Function Declaration : int a3_Game(int delay, char *pattern, int target)
@@ -33,158 +252,38 @@
 
 @ Here is the actual a3_Game function
 
-a3_Game:
+lab6:
 
-    push    {r4-r11, lr}                    @ Put aside registers we want to restore later
+    push    {r4-r7, lr}                     @ Put aside registers we want to restore later
 
-    mov     r4, #13421                      @ Set aside r4 to use for delay
-
-    mul     r4, r0                          @ multiply our delay value to scale it
-
-    mov     r9, r1                          @ copy the pattern into r9
-
-    mov     r6, r2                          @ Set aside r6 to use for target
-
-    mov     r8, #0                          @ copy 0 into r8 to use for the offset counter
-
-    mov     r10, #0                         @ copy 0 into r10 to use for winning led counter loop
-
-    mov     r11, #2                         @ copy 2 into r11 for blink twice counter
-
-turn_all_off:
-
-    mov     r0, r10                         @ copy the led counter index into r10
-    bl      BSP_LED_Off                     @ call the BSP_LED_Off function
-
-    add     r10, r10, #1                    @ add 1 to the led counter
-
-    cmp     r10, #8                         @ compare to see if the final led is reached
-    blt     turn_all_off                    @ branch back to turn_all_off is the led counter is less than 8
-
-    mov     r10, #0                         @ copy 0 into r10 to reset the counter for later use
-
-
-    pattern_loop: 
-
-        ldrb    r5, [r9, r8]                @ Dereference the character r9 points to
     
 
-        cmp     r5, #0                      @ compare the ascii value of the current offset to 0 to see if it is null
-        beq     pattern_checkpoint          @ if the value is null go to pattern_checkpoint, continue if not
+    @ldr     r0, =READ_DELAY                 @ load delay into r0 for busy_delay
+    @bl      busy_delay                      @ call busy_delay
 
 
-        mov     r7, r5                      @ Copy the ascii value of r5 to r7
-        sub     r7, r7, #48                 @ subtract 48 from r7 to obtain the led index
+    mov     r0, #I2C_Address                @ copy I2C address into r0
+    mov     r1, #X_HI                       @ use r1 to hold high bit value of accelerometer
+    bl      COMPASSACCELERO_IO_Read         @ call compass accelero to read input of accelerometer
 
+    sxtb    r0, r0                           @ extend the signed 8 bit value in r0 to be a signed 32 bit value
 
-        mov     r0, r7                      @ r0 holds our argument for the LED toggle function, so pass I pass the index (r7)
-        bl      BSP_LED_Toggle              @ call the led toggle function to turn it on/off
-
-
-        mov     r0, r4                      @ copy our delay to r0 so the busy delay can use the delay value
-        bl      busy_delay                  @ call the busy_delay function to add a delay between the next toggle
-
-
-        bl      BSP_PB_GetState             @ call BSP_PB_GetState
-        cmp     r0, #1                      @ check to see if the button is pressed
-        beq     button_check                @ if the button is pressed go to button_check
-
-
-        mov     r0, r7                      @ r0 holds our argument for the LED toggle function, so pass I pass the index (r7)
-        bl      BSP_LED_Toggle              @ call the led toggle function to turn it on/off
-
-
-        mov     r0, r4                      @ copy our delay to r0 so the busy delay can use the delay value
-        bl      busy_delay                  @ call the busy_delay function to add a delay between the next toggle
-
-
-        add     r8, r8, #1                  @ add 1 to the offset of the string array
-
-
-    pattern_checkpoint:
-
-        cmp     r5, #0                      @ compare r5 to 0 to see if end of string is reached 
-        bgt     pattern_loop                @ branch back to pattern_loop if r5 is greater than 0
-
-
-        mov     r8, #0                      @ reset the offset value to 0 to create infinite loop
-        bl      pattern_loop                @ branch back to pattern_loop
-
-
-
-button_check:
-
-    mov     r0, r7                          @ copy current led index to r7
-    bl      BSP_LED_Toggle                  @ toggle the specified light on
-
-
-    cmp     r6, r7                          @ compare to see if target led is the same as current led
-    beq     win_led_loop_on                 @ if equal, go to win condition (win_led_loop_on)
-
-
-    cmp     r6, r7                          @ compare to see if target led is the same as current led
-    bne     lose_led_on                     @ if not equal, go to lose condition (lose_led_on)
-
-
-
-    win_led_loop_on:
-
-
-        mov     r0, r10                     @ r0 holds our argument for the LED toggle function, so pass I pass the index (r10)
-        bl      BSP_LED_Toggle              @ call the led toggle function to turn it on/off
-
-
-        add     r10, r10, #1                @ add 1 to the led counter
-
-
-        cmp     r10, #8                     @ compare r10 to 8 to check if the led counter is on the last led
-        blt     win_led_loop_on             @ if the led counter is less than 8, branch back to win_led_loop
-
-
-        ldr     r0, =0x666666               @ copy our delay to r0 so the busy delay can use the delay value
-        bl      busy_delay                  @ call the busy_delay function to add a delay between the next toggle
-
-
-        mov     r10, #0                     @ copy 0 to r10 to reset our counter for the led
-    
-
-    win_led_loop_off:
-
-        mov     r0, r10                     @ r0 holds our argument for the LED toggle function, so pass I pass the index (r10)
-        bl      BSP_LED_Toggle              @ call the led toggle function to turn it on/off
-
-
-        add     r10, r10, #1                @ add 1 to the led counter
-
-
-        cmp     r10, #8                     @ compare r10 to 8 to check if the led counter is on the last led
-        blt     win_led_loop_off            @ if the led counter is less than 8, branch back to win_led_loop
-
-
-        ldr     r0, =0x666666               @ copy our delay to r0 so the busy delay can use the delay value
-        bl      busy_delay                  @ call the busy_delay function to add a delay between the next toggle
-
-
-        mov     r10, #0                     @ copy 0 to r10 to reset our counter for the led
-
-
-        subs     r11, r11, #1               @ subtract one from the twice blink counter
-        bgt     win_led_loop_on             @ branch back to win loop if the value if greater than 0
-
-        b       exit_program                @ branch to exit_program
-
-
-    lose_led_on:
-
-        mov     r0, r6                      @ r0 holds our argument for the LED toggle function, so pass I pass the index (r6)
-        bl      BSP_LED_Toggle              @ call the led toggle function to turn it on/off
 
 
 exit_program:
 
-    pop     {r4-r11, lr}                    @ Bring all the register values back
+    pop     {r4-r7, lr}                     @ Bring all the register values back
 
     bx      lr                              @ Return (Branch eXchange) to the address in the link register (lr)
+
+
+
+
+
+
+
+
+
 
 
 
@@ -215,6 +314,6 @@ delay_1oop:
 
 
       
-.size a3_Game, .-a3_Game  @@ - symbol size (not strictly required, but makes the debugger happy)
+.size lab6, .-lab6  @@ - symbol size (not strictly required, but makes the debugger happy)
 
 .end                                    @ Assembly file ended by single .end directive on its own line
--- an_hook.c
+++ an_hook.c
@@ -17,17 +17,166 @@
 #include "common.h"
 
 // Prototype for the blinking game function
-int a3_Game(int delay, char *pattern, int target);
+//int a3_Game(int delay, char *pattern, int target);
 
 
-void _an_A3(int action)
+
+//void _an_A3(int action)
+//{
+//
+//  // The prompt that is shown when the user uses the help command with anGame 
+//  if(action==CMD_SHORT_HELP) return;
+//  if(action==CMD_LONG_HELP) {
+//    printf("Play the blinking lights game!\n\n"
+//	   "This function will allow you to play the blinking lights game\n"
+//	   );
+//
+//    return;
+//  }
+//
+//
+//
+//  // first argument for the delay parameter
+//  uint32_t delay;
+//
+//  int fetch_status;
+//
+//  fetch_status = fetch_uint32_arg(&delay);
+//
+//  if(fetch_status) {
+//  	// default delay
+//  	delay = 0x1F4;
+//  }
+//
+//
+//
+//  // Second argument for the pattern
+//  char *pattern;
+//
+//  fetch_status = fetch_string_arg(&pattern);
+//
+//  if(fetch_status) {
+//      // Default pattern
+//      pattern = "43567011";
+//  }
+//
+//
+//  // Third argument for the target parameter
+//  uint32_t target;
+//
+//  fetch_status = fetch_uint32_arg(&target);
+//
+//  if(fetch_status) {
+//  	// default target
+//  	target = 6;
+//  }
+//
+//
+//  // Call the game function
+//  a3_Game(delay, pattern, target);
+//
+//  printf("Play again?\n\n");
+//
+//
+//}
+//ADD_CMD("anGame", _an_A3,"Play the blinking lights game")
+
+
+int lab6(int user_input);
+
+void _an_lab6(int action)
 {
 
-  // The prompt that is shown when the user uses the help command with anGame 
+  // The prompt that is shown when the user uses the help command with lab6 
   if(action==CMD_SHORT_HELP) return;
   if(action==CMD_LONG_HELP) {
-    printf("Play the blinking lights game!\n\n"
-	   "This function will allow you to play the blinking lights game\n"
+    printf("Check accelerometer\n\n"
+	   "This function will allow you to check the accelerometer\n"
+	   );
+
+    return;
+  }
+
+  uint32_t user_input;
+
+  int fetch_status;
+
+  fetch_status = fetch_uint32_arg(&user_input);
+
+  if(fetch_status) {
+  	// default target
+  	user_input = 0;
+  }
+
+
+  for (int i = 0; i < 100; i++)
+  {
+    printf("Accel Value: %d\n\n", lab6(user_input));
+  }
+
+
+
+}
+ADD_CMD("lab6", _an_lab6,"Accelerometer Test")
+
+
+
+
+
+int _an_lab_setup(int lab7_input);
+
+
+void _an_lab7(int action)
+{
+
+  // The prompt that is shown when the user uses the help command with lab7 
+  if(action==CMD_SHORT_HELP) return;
+  if(action==CMD_LONG_HELP) {
+    printf("Tick Handler\n\n"
+	   "This function will allow you to play with the tick handler\n"
+	   );
+
+    return;
+  }
+
+  uint32_t lab7_input;
+
+  int fetch_status;
+
+  fetch_status = fetch_uint32_arg(&lab7_input);
+
+  if(fetch_status) {
+  	// default value
+  	lab7_input = 5000;
+  }
+
+
+  
+  printf("Tick handler: %d\n\n", _an_lab_setup(lab7_input));
+  
+
+
+
+}
+ADD_CMD("lab7", _an_lab7,"Tick Handler")
+
+
+
+
+
+
+
+
+int _an_lab_tick();
+
+void lab7tick(int action)
+{
+
+  // The prompt that is shown when the user uses the help command with lab7 
+  if(action==CMD_SHORT_HELP) return;
+  if(action==CMD_LONG_HELP) {
+    printf("Tick Handler\n\n"
+	   "This function will allow you to play with the tick handler\n"
 	   );
 
     return;
@@ -35,48 +184,11 @@ void _an_A3(int action)
 
 
 
-  // first argument for the delay parameter
-  uint32_t delay;
+  
+  printf("Tick handler: %d\n\n", _an_lab_tick());
+  
 
-  int fetch_status;
-
-  fetch_status = fetch_uint32_arg(&delay);
-
-  if(fetch_status) {
-  	// default delay
-  	delay = 0x1F4;
-  }
-
-
-
-  // Second argument for the pattern
-  char *pattern;
-
-  fetch_status = fetch_string_arg(&pattern);
-
-  if(fetch_status) {
-      // Default pattern
-      pattern = "43567011";
-  }
-
-
-  // Third argument for the target parameter
-  uint32_t target;
-
-  fetch_status = fetch_uint32_arg(&target);
-
-  if(fetch_status) {
-  	// default target
-  	target = 6;
-  }
-
-
-  // Call the game function
-  a3_Game(delay, pattern, target);
-
-  printf("Play again?\n\n");
 
 
 }
-ADD_CMD("anGame", _an_A3,"Play the blinking lights game")
-
+ADD_CMD("ticktest", lab7tick,"Tick Handler")
