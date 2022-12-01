027c4faebeb26e3f54e0406b64797af8e1c2f5e6
Amandeep <anath0610@conestogac.on.ca>
2022-12-01 00:58:53 -0500

    Infinite blinking of LED's is now working.

--- an_asm.s
+++ an_asm.s
@@ -3,7 +3,9 @@
 .data
 
 a5_timeout: .word 0
-a5_delay: .word 0
+a5_on_delay: .word 0
+a5_off_delay: .word 0
+a5_reset_delay: .word 0
 
 LEDaddress: .word 0x48001014
 
@@ -58,48 +60,20 @@ _an_watchdog_start:
     str     r0, [r3]                        @ store timeout into a5_timeout
 
 
-    ldr     r3, =a5_delay                   @ load address into r3 so parameters don't get overwritten
-    str     r1, [r3]                        @ store delay into a5_delay
+    ldr     r3, =a5_on_delay                @ load address into r3 so parameters don't get overwritten
+    str     r1, [r3]                        @ store delay into a5_on_delay
+
+    ldr     r3, =a5_off_delay               @ load address into r3 so parameters don't get overwritten
+    str     r1, [r3]                        @ store delay into a5_off_delay
+
+    ldr     r3, =a5_reset_delay             @ load address into r3 so parameters don't get overwritten
+    str     r1, [r3]                        @ store delay into a5_reset_delay
+
+
+    bx      lr                              @ Return (Branch eXchange) to the address in the link register (lr)
 
 
 
-
-
-
-
-
-
-@ldr     r3, =a4_accel_ticks             @ load address into r3 so parameters don't get overwritten
-@mov     r0, #accel_delay                @ copy value of accel_delay into r0
-@str     r0, [r3]                        @ store target into a4_accel_ticks
-@
-@
-@ldr     r1, =a4_ticks                   @ get the address of a4_ticks
-@mov     r3, #game_time_constant         @ copy 1000 to r3 to use for tick timer
-@mul     r2, r3                          @ multiply game_time by 1000 to convert into seconds
-@str     r2, [r1]                        @ store game time into a4_ticks
-@
-@
-@bx      lr                              @ Return (Branch eXchange) to the address in the link register (lr)
-
-
-
-
-
-
-
-
-@@@@@@@@@@@@@@@@@@@@@@@@@ Code to turn on all LED's, use later @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-    @ This code turns on only one light – can you make it turn them all on at once?
-    @ldr     r1, =LEDaddress	@ Load the GPIO address we need
-    @ldr     r1, [r1]		@ Dereference r1 to get the value we want
-    @ldrh    r0, [r1]		@ Get the current state of that GPIO (half word only)
-
-    @orr     r0, r0, #0xFF00		@ Use bitwise OR (ORR) to set the bit at 0x0100
-    @strh    r0, [r1]		@ Write the half word back to the memory address for the GPIO
-
-    @bx      lr
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 
 @@@@@@@@@@@@@@@@@@@@@@@@@ Use code for later @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -110,133 +84,186 @@ _an_watchdog_start:
 
 
 
+@ Test code for my own new function called from C
+@ This is a comment. Anything after an @ symbol is ignored.
+@@ This is also a comment. Some people use double @@ symbols.
+    .code 16                            @ This directive selects the instruction set being generated.
+                                        @ The value 16 selects Thumb, with the value 32 selecting ARM.
+
+    .text                               @ Tell the assembler that the upcoming section is to be considered
+                                        @ assembly language instructions - Code section (text -> ROM)
+
+
+
+@@ Function Header Block
+    .align 2                            @ Code alignment - 2^n alignment (n=2)
+                                        @ This causes the assembler to use 4 byte alignment
+
+    .syntax unified                     @ Sets the instruction set to the new unified ARM + THUMB
+                                        @ instructions. The default is divided (separate instruction sets)
+
+    .global _an_a5_tick_handler          @ Make the symbol name for the function visible to the linker
+
+    .code 16                            @ 16bit THUMB code (BOTH .code and .thumb_func are required)
+    .thumb_func                         @ Specifies that the following symbol is the name of a THUMB
+                                        @ encoded function. Necessary for interlinking between ARM and THUMB code.
+
+    .type _an_a5_tick_handler, %function     @ Declares that the symbol is a function (not strictly required)
 
 
 
 
-@_an_a4_tick:
-@
-@    push    {r4-r6, lr}                     @ Put aside registers we want to restore later
-@
-@
-@    ldr     r1, =a4_ticks                   @ load address of a4_ticks into r1
-@    ldr     r0, [r1]                        @ load current ticks value 
-@    subs    r0, r0, #1                      @ subtract 1 from the ticks
-@    ble     exit_tick                       @ go to exit_tick if it is 0
-@    str     r0, [r1]                        @ store value back into r1
-@
-@
-@    cmp     r0, #1                          @ compare a4 ticks to something greater than 0
-@    beq     lose_led                        @ if the values are equal go to lose_led
-@    
-@
-@    ldr     r1, =a4_accel_ticks             @ load address of a4_accel_ticks into r1
-@    ldr     r0, [r1]                        @ load current a4_accel_ticks ticks value
-@    subs    r0, r0, #1                      @ subtract 1 from a4_accel_ticks
-@    str     r0, [r1]                        @ store the current decremented count
-@    bgt     exit_tick                       @ go to exit_tick if the tick isn't 0
-@
-@    
-@    bl      accelero_check                  @ go to accelero_check to see which led to turn on
-@
-@    mov     r6, r0                          @ copy LED index into r6 incase we need to use later
-@
-@    ldr     r1, =a4_target                  @ load address of a4_target into r1
-@    ldr     r4, [r1]                        @ load the traget into r4 to use later
-@
-@    cmp     r6, r4                          @ compare LED index to target
-@    bne     exit_target_check               @ if they are not equal, go to exit_target_check
-@
-@
-@    ldr     r1, =a4_delay                   @ load address of a4_delay
-@    ldr     r0, [r1]                        @ load current delay ticks value
-@    subs    r0, r0, #1                      @ subtract 1 from ticks
-@    str     r0, [r1]                        @ store the current decremented count
-@    bgt     exit_tick                       @ branch to exit_tick if value is greater than 0
-@
-@
-@    ldr     r1, =a4_ticks                   @ load address of a4_ticks
-@    mov     r0, #0                          @ copy 0 to r0 since we have won the game
-@    str     r0, [r1]                        @ store value back into a4_ticks
-@
-@    bl      win                             @ branch to win to flash LED's
-@
-@    bl      exit_tick                       @ branch to exit_tick since we are done
-@
-@
-@
-@exit_target_check:
-@
-@    ldr     r1, =a4_reset_delay             @ load address of a4_reset_delay into r1
-@    ldr     r0, [r1]                        @ load the reset delay into r0 to use later
-@    ldr     r1, =a4_delay                   @ load address of a4_delay
-@    str     r0, [r1]                        @ store the reset value into a4_delay
-@
-@
-@
-@    ldr     r1, =a4_accel_ticks             @ load address of a4_accel_ticks
-@    mov     r0, #accel_delay                @ reset the accel tick value
-@    str     r0, [r1]                        @ store the value into a4_accel_ticks
-@
-@    bl      exit_tick                       @ branch to exit tick
-@
-@
-@lose_led:
-@
-@
-@    bl      lose                            @ call lose function
-@
-@
-@exit_tick:
-@
-@
-@    pop     {r4-r6, lr}                    @ Bring all the register values back
-@
-@    bx      lr                             @ Return (Branch eXchange) to the address in the link register (lr)
+_an_a5_tick_handler:
+
+    push    {lr}                            @ Put aside registers we want to restore later
+
+
+    ldr     r1, =a5_on_delay                @ load address of a5_on_delay into r1
+    ldr     r0, [r1]                        @ load current ticks value 
+    subs    r0, r0, #1                      @ subtract 1 from the ticks
+    str     r0, [r1]                        @ store value back into r1
+    bgt     exit_tick                       @ go to exit_tick if it is greater than 0
 
 
 
+    ldr     r1, =LEDaddress	                @ Load the GPIO address we need
+    ldr     r1, [r1]		                @ Dereference r1 to get the value we want
+    ldrh    r0, [r1]		                @ Get the current state of that GPIO (half word only)
+
+    orr     r0, r0, #0xFF00		            @ Use bitwise OR (ORR) to set the bit at 0x0100
+    strh    r0, [r1]		                @ Write the half word back to the memory address for the GPIO
 
 
 
+    ldr     r1, =a5_off_delay               @ load address of a4_off_delay into r1
+    ldr     r0, [r1]                        @ load current ticks value 
+    subs    r0, r0, #1                      @ subtract 1 from the ticks
+    str     r0, [r1]                        @ store value back into r1
+    bgt     exit_tick                       @ go to exit_tick if it is greater than 0
 
 
 
+    ldr     r1, =LEDaddress	                @ Load the GPIO address we need
+    ldr     r1, [r1]		                @ Dereference r1 to get the value we want
+    ldrh    r0, [r1]		                @ Get the current state of that GPIO (half word only)
 
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    LAB 8    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-
-
-
-
-
+    and     r0, r0, #0x00FF		            @ Use bitwise AND to set the bit at 0x00FF
+    strh    r0, [r1]		                @ Write the half word back to the memory address for the GPIO
 
 
 
+    ldr     r2, =a5_reset_delay             @ load address of a5_reset_delay into r2
+    ldr     r0, [r2]                        @ load value into r0
+
+    ldr     r1, =a5_on_delay                @ load address of a5_on_delay into r1
+    str     r0, [r1]                        @ store value of reset into r1
+
+    ldr     r1, =a5_off_delay               @ load address of a5_off_delay into r1
+    str     r0, [r1]                        @ store value reset into r1
 
 
+exit_tick:
 
 
+    pop     {lr}                            @ Bring all the register values back
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    bx      lr                              @ Return (Branch eXchange) to the address in the link register (lr)
 
 
     
 
 
+
+
+
+
+
+
+@ Test code for my own new function called from C
+@ This is a comment. Anything after an @ symbol is ignored.
+@@ This is also a comment. Some people use double @@ symbols.
+    .code 16                            @ This directive selects the instruction set being generated.
+                                        @ The value 16 selects Thumb, with the value 32 selecting ARM.
+
+    .text                               @ Tell the assembler that the upcoming section is to be considered
+                                        @ assembly language instructions - Code section (text -> ROM)
+
+
+
+@@ Function Header Block
+    .align 2                            @ Code alignment - 2^n alignment (n=2)
+                                        @ This causes the assembler to use 4 byte alignment
+
+    .syntax unified                     @ Sets the instruction set to the new unified ARM + THUMB
+                                        @ instructions. The default is divided (separate instruction sets)
+
+    .global lab8          @ Make the symbol name for the function visible to the linker
+
+    .code 16                            @ 16bit THUMB code (BOTH .code and .thumb_func are required)
+    .thumb_func                         @ Specifies that the following symbol is the name of a THUMB
+                                        @ encoded function. Necessary for interlinking between ARM and THUMB code.
+
+    .type lab8, %function     @ Declares that the symbol is a function (not strictly required)
+
+lab8:
+
+    ldr     r1, =LEDaddress	                @ Load the GPIO address we need
+    ldr     r1, [r1]		                @ Dereference r1 to get the value we want
+    ldrh    r0, [r1]		                @ Get the current state of that GPIO (half word only)
+
+    orr     r0, r0, #0xFF00		            @ Use bitwise OR (ORR) to set the bit at 0x0100
+    strh    r0, [r1]		                @ Write the half word back to the memory address for the GPIO
+
+
+    bx      lr 
+
+
+
+
+
+
+
+
+
+
+
+@ Test code for my own new function called from C
+@ This is a comment. Anything after an @ symbol is ignored.
+@@ This is also a comment. Some people use double @@ symbols.
+    .code 16                            @ This directive selects the instruction set being generated.
+                                        @ The value 16 selects Thumb, with the value 32 selecting ARM.
+
+    .text                               @ Tell the assembler that the upcoming section is to be considered
+                                        @ assembly language instructions - Code section (text -> ROM)
+
+
+
+@@ Function Header Block
+    .align 2                            @ Code alignment - 2^n alignment (n=2)
+                                        @ This causes the assembler to use 4 byte alignment
+
+    .syntax unified                     @ Sets the instruction set to the new unified ARM + THUMB
+                                        @ instructions. The default is divided (separate instruction sets)
+
+    .global lab81          @ Make the symbol name for the function visible to the linker
+
+    .code 16                            @ 16bit THUMB code (BOTH .code and .thumb_func are required)
+    .thumb_func                         @ Specifies that the following symbol is the name of a THUMB
+                                        @ encoded function. Necessary for interlinking between ARM and THUMB code.
+
+    .type lab81, %function     @ Declares that the symbol is a function (not strictly required)
+
+lab81:
+
+    ldr     r1, =LEDaddress	                @ Load the GPIO address we need
+    ldr     r1, [r1]		                @ Dereference r1 to get the value we want
+    ldrh    r0, [r1]		                @ Get the current state of that GPIO (half word only)
+
+    and     r0, r0, #0x00FF		            @ Use bitwise OR (ORR) to set the bit at 0x0100
+    strh    r0, [r1]		                @ Write the half word back to the memory address for the GPIO
+
+
+    bx      lr
 
 
 .size _an_watchdog_start, .-_an_watchdog_start  @@ - symbol size (not strictly required, but makes the debugger happy)
--- an_hook.c
+++ an_hook.c
@@ -16,6 +16,55 @@
 #include "common.h"
 #include "watchdog.h"
 
+int lab8();
+
+void _an_lab8(int action)
+{
+
+  // The prompt that is shown when the user uses the help command with lab8test 
+  if(action==CMD_SHORT_HELP) return;
+  if(action==CMD_LONG_HELP) {
+    printf("Lab 8 watchdog\n\n"
+	   "This function will allow you to test lab8 watchdog\n"
+	   );
+
+    return;
+  }
+
+  lab8();
+
+  printf("Play Again?\n\n");
+  
+
+}
+ADD_CMD("lab8", _an_lab8,"Lab8")
+
+
+int lab81();
+
+void _an_lab81(int action)
+{
+
+  // The prompt that is shown when the user uses the help command with lab8test 
+  if(action==CMD_SHORT_HELP) return;
+  if(action==CMD_LONG_HELP) {
+    printf("Lab 8 watchdog\n\n"
+	   "This function will allow you to test lab8 watchdog\n"
+	   );
+
+    return;
+  }
+
+  lab81();
+
+  printf("Play Again?\n\n");
+  
+
+}
+ADD_CMD("lab81", _an_lab81,"Lab8")
+
+
+
 
 
 void _an_lab8test(int action)
@@ -87,7 +136,7 @@ void _an_A5(int action)
 
   if(fetch_status) {
   	// Use a default value
-  	timeout = 500;
+  	timeout = 100;
   }
 
 
@@ -101,12 +150,12 @@ void _an_A5(int action)
   	delay = 500;
   }
 
-  //hiwdg.Init.Reload = timeout;
+ 
 
-  mes_InitIWDG(timeout);
+  //mes_InitIWDG(timeout);
   
 
-  mes_IWDGStart();
+  //mes_IWDGStart();
 
   _an_watchdog_start(timeout, delay);
 
@@ -115,3 +164,31 @@ void _an_A5(int action)
 
 }
 ADD_CMD("anWatch", _an_A5,"Stop refreshing watchdog")
+
+
+
+
+
+int _an_a5_tick_handler();
+
+
+void _an_A5_tick(int action)
+{
+
+  // The prompt that is shown when the user uses the help command with anWatch 
+  if(action==CMD_SHORT_HELP) return;
+  if(action==CMD_LONG_HELP) {
+    printf("Watchdog for A5\n\n"
+	   "This command stops the watchdog refresh\n"
+	   );
+
+    return;
+  }
+
+  _an_a5_tick_handler();
+
+  printf("Stopped Watchdog\n\n");
+
+
+}
+ADD_CMD("anTick", _an_A5_tick,"Stop refreshing watchdog")
